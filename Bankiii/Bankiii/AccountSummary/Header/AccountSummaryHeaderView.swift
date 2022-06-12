f61d21f296eaec395be45fb3f7ac8192e042c31d
joelzarco <joelzarco@me.com>
2022-06-12 16:51:50 -0500

    Feature: Networking to account/profile added

--- Bankiii/Bankiii/AccountSummary/Header/AccountSummaryHeaderView.swift
+++ Bankiii/Bankiii/AccountSummary/Header/AccountSummaryHeaderView.swift
@@ -10,8 +10,21 @@ import UIKit
 class AccountSummaryHeaderView : UIView{
     
     @IBOutlet var contentView: UIView!
+    @IBOutlet weak var welcomeLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
     
     let shakeyBellView = ShakeyBellView()
+    
+    //viewModel to represent header aproperties
+    struct ViewModel{
+        let welcomeMessage : String
+        let name : String
+        let date : Date
+        var dateFormatted : String{
+            return date.monthDayYearString
+        }
+    }
     
     override init(frame: CGRect) {
             super.init(frame: frame)
@@ -48,5 +61,11 @@ class AccountSummaryHeaderView : UIView{
         addSubview(shakeyBellView)
         shakeyBellView.trailingAnchor.constraint(equalTo: trailingAnchor).isActive = true
         shakeyBellView.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+    }
+    
+    func configure(viewModel : ViewModel){
+        welcomeLabel.text = viewModel.welcomeMessage
+        nameLabel.text = viewModel.name
+        dateLabel.text = viewModel.dateFormatted
     }
 }
